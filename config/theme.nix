5f8fa6b9ba90efc085c0782f5fda7ce74cbb28ae
Tom Butler <tom.butler@lendable.co.uk>
2024-11-07 14:31:34 +0000

    Add neovide scale

--- config/theme.nix
+++ config/theme.nix
@@ -36,6 +36,16 @@
       vim.cmd[[colorscheme nord]]
 
       vim.g.neovide_transparency = 0.5
+      vim.g.neovide_scale_factor = 1.0
+      local change_scale_factor = function(delta)
+        vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+      end
+      vim.keymap.set("n", "<C-=>", function()
+        change_scale_factor(1.1)
+      end)
+      vim.keymap.set("n", "<C-->", function()
+        change_scale_factor(1/1.1)
+      end)
     end
 
     vim.api.nvim_set_hl(0, "@variable", { fg = frost3 })
