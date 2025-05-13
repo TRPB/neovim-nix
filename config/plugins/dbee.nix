116c63e0dfc7f7c54fd17fcf74cbf90debd72088
Tom Butler <tom.butler@lendable.co.uk>
2025-05-13 20:08:50 +0100

    install cmp-dbee

--- config/plugins/dbee.nix
+++ config/plugins/dbee.nix
@@ -1,6 +1,15 @@
 { pkgs, ... }:
 {
-  extraPlugins = [ pkgs.vimPlugins.nvim-dbee ];
+  extraPlugins = [
+    pkgs.vimPlugins.nvim-dbee
+
+    (pkgs.fetchFromGitHub {
+      owner = "MattiasMTS";
+      repo = "cmp-dbee";
+      rev = "1650f67b9bf43c029fc37570665ca895a33cdf5a";
+      sha256 = "sha256-XxB4jQu9xAi/7XDcwsd0hGLSs74ysjg0N/uaTHjqByI=";
+    })
+  ];
   keymaps = [
     {
       mode = [ "n" ];
@@ -9,4 +18,20 @@
       options.silent = true;
     }
   ];
+
+  extraConfigLua = ''
+    local auGroup = vim.api.nvim_create_augroup("nvim-dbee-custom", {clear = true})
+    vim.api.nvim_create_autocmd("FileType", {
+      group = auGroup,
+      pattern = "sql",
+      callback = function()
+        require('cmp').setup.filetype({"sql", "mysql", "plsql"}, {
+            sources = require('cmp').config.sources{
+                { name = 'cmp-dbee' }
+            }
+        })
+        require("cmp-dbee").setup({})
+      end
+    })
+  '';
 }
